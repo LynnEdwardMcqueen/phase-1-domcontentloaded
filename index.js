64df32a40e57f6f3af08335985d54e59bab93a37
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-04-12 13:54:04 -0600

    Adds DOMContentLoaded listener and alters paragraph message

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let paragraph = document.getElementById("text");
+    paragraph.innerHTML = "This is really cool!"
+})
